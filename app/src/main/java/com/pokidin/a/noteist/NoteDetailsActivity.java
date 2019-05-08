eb9d904ddff44f2cf73897df8ed4026c78d084f7
AlexanderPokidin <a.pokidin@gmail.com>
2019-05-08 15:12:28 +0300

    Base version application completed. Small fixes

--- app/src/main/java/com/pokidin/a/noteist/NoteDetailsActivity.java
+++ app/src/main/java/com/pokidin/a/noteist/NoteDetailsActivity.java
@@ -3,9 +3,12 @@ package com.pokidin.a.noteist;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.pokidin.a.noteist.db.NoteDatabase;
 import com.pokidin.a.noteist.entity.Note;
@@ -42,10 +45,10 @@ public class NoteDetailsActivity extends AppCompatActivity {
 
     private void createNewNoteWithUserText() {
         Intent intent = getIntent();
-        // If the Extra argument is larger than 0, fill in the EditText`s field.
+        // If the Extra argument is larger than -1, fill in the EditText`s field.
         // Otherwise, leave the field blank.
-        int position = intent.getIntExtra(MainActivity.POSITION, 0);
-        if (position > 0) {
+        int position = intent.getIntExtra(MainActivity.POSITION, -1);
+        if (position >= 0) {
             String currentText = mDatabase.getNoteAtPosition(position).getText();
             mText.setText(currentText);
             mDatabase.removeNoteAtPosition(position);
@@ -59,5 +62,31 @@ public class NoteDetailsActivity extends AppCompatActivity {
         }
         Intent intent = new Intent(NoteDetailsActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.delete_detail, menu);
+        getMenuInflater().inflate(R.menu.share_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_delete) {
+            testToast(" Delete");
+            return true;
+        }
+        if (id == R.id.action_share) {
+            testToast(" Share");
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void testToast(String s) {
+        Toast toast = Toast.makeText(this, getClass().getSimpleName() + s, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
